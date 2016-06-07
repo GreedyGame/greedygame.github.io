377a1f7bbe84810ec06030a5f056b5e3648ff3c6
niks_nixac <nikhil@greedygame.com>
2016-06-07 12:12:52 +0530

    added side navi menu

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 console.log('This would be the main JS file.');
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
