e8f50dafbbad137ef50720ec43cf407ead7b944a
niks_nixac <nikhil@greedygame.com>
2016-06-07 13:02:53 +0530

    javascript not working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 console.log('This would be the main JS file.');
 
 
-/* Set the width of the side navigation to 250px */
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
